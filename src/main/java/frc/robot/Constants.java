2f99b94183a00d31a2e0affc74b80c35a271e508
Andres Ruiz <Andres.ruiz3561@gmail.com>
2021-12-21 18:13:29 -0800

    i made some changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,6 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static double kP = 0.3, kI = 0.11, kD = 0;
+    public static double kP = 0.1, kI = 0.01, kD = 0;
     public static int angel = 0;
 }
